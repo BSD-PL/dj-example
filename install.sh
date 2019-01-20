926babd10e360982929130836eb8f1fb16e527f8
Michał Borysiak <borysiam@gmail.com>
2019-01-20 23:59:43 +0100

    Add a simple install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+PREFIX="/usr/local"
+FILE="$(realpath "${0}")"
+HERE="$(dirname "${FILE}")"
+
+#python setup.py install
+mkdir -p "${PREFIX}/etc/nginx"
+mkdir -p "${PREFIX}/etc/uwsgi"
+
+ln -fs "${HERE}/etc/nginx.conf" "${PREFIX}/etc/nginx/nginx.conf"
+ln -fs "${HERE}/etc/uwsgi.ini"  "${PREFIX}/etc/uwsgi/uwsgi.ini"
+
+sysrc nginx_enable="YES"
+sysrc uwsgi_enable="YES"
