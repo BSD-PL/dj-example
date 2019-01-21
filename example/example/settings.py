791b161a610d355a208a93327a9e8f95a15dc706
Michał Borysiak <borysiam@gmail.com>
2019-01-21 15:47:01 +0000

    Add logging configuration

--- example/example/settings.py
+++ example/example/settings.py
@@ -124,3 +124,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': None,
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
