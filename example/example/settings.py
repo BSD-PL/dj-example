436c5bc48a4fe8144c4f520d2864d509e55028d3
Michał Borysiak <borysiam@gmail.com>
2019-01-21 15:44:54 +0000

    Use Postgres as a database server

--- example/example/settings.py
+++ example/example/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'example.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'example',
+        'USER': 'example',
+        'PASSWORD': 'example',
+        'HOST': '10.0.2.16',
+        'PORT': '5432',
     }
 }
 
