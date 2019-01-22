7d25cd279280366e46b1a0dfd35ded7bb0070341
Michał Borysiak <borysiam@gmail.com>
2019-01-22 01:13:02 +0100

    Enable static files

--- example/example/settings.py
+++ example/example/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u8)db%q&choy(p7v0$w@-7h&&&6!nwh(+x1r1x66zld5s49to4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
