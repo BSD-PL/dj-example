c3a069c3fb293f1916de71336c38c9eab8f6a97e
borysiam <borysiam@gmail.com>
2019-01-24 11:45:59 +0100

    Finish the polls tutorial

--- example/urls.py
+++ example/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
+from django.urls import include, path
 from django.contrib import admin
 
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
